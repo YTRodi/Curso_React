bc04297b82eb5a6061843d8eec28bc68cfe2c8e7
YTRodi <yagorodi@gmail.com>
2020-10-29 11:52:30 -0300

    feat: intro a React.mouseEvent

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ( { value } ) => {
 
+    const handleAdd = ( e ) => { console.log( e ); }
+
     return (
 
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            {/* El argumento del evento onClick podemos pasarlos de dos formas */}
+
+            {/* #1 - Referencia a una función */}
+            {/* <button onClick={ ( e ) => { handleAdd( e ) } }>+1</button> */}
+
+            {/* #2 - Obviamos el parámetro del evento onClick */}
+            <button onClick={ handleAdd }>+1</button>
 
         </>
 
