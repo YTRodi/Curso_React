cd913927be119547e1067de4d17a73aebc75fea3
YTRodi <yagorodi@gmail.com>
2020-10-29 11:10:56 -0300

    feat: default props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,21 +1,34 @@
-// Regla de estilos: Cada componente debe escribirse con el UpperCamelCase
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
 
 // Functional Components ( Regresa un component de HTML )
-const PrimeraApp = ( { saludo } ) => {
-
-    // Usamos destructuración para los elementos. ( properties/props )
-    // console.log(props);
+const PrimeraApp = ( { saludo, subtitulo } ) => {
     
     return (
 
         <>
             <h1> { saludo } </h1>
-            <p>Mi primera aplicación</p>            
+            <p> { subtitulo } </p>            
         </>
        
     ); 
 
 };
+
+// Si alguien quiere usar este componente, tiene que proveer la propiedad 'saludo'.
+PrimeraApp.propTypes = {
+
+    // Le digo que el saludo es un string y es obligatorio
+    saludo: PropTypes.string.isRequired
+
+}
+
+// Si queremos que algunas propiedades traigan por default
+PrimeraApp.defaultProps = {
+
+    subtitulo: 'Soy un subtitulo'
+
+}
+
 
 export default PrimeraApp;
