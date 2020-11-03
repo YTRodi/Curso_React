92b9aa79a945794cf72794f1e892c13bfe4ba073
YTRodi <yagorodi@gmail.com>
2020-11-02 22:24:56 -0300

    feat: first test

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,17 +1,13 @@
 //! Agrupamos las pruebas unitarias.
-describe( 'Pruebas en el archivo de demo.test.js', () => {
-
-    test( 'Deben de ser iguales los strings', () => {
-    
+describe('Pruebas en el archivo de demo.test.js', () => {
+    test('Deben de ser iguales los strings', () => {
         // 1 - Arrange ( Inicialización )
         const msg = 'Hola Mundo';
-    
+
         // 2 - Act ( Estímulo )
         const msg2 = `Hola Mundo!`;
-        
-        // 3 - Assert ( Observar comportamiento )
-        expect( msg ).toBe( msg2 ); // Internamente hace un ===
-    
-    });
 
+        // 3 - Assert ( Observar comportamiento )
+        expect(msg).toBe(msg2); // Internamente hace un ===
+    });
 });
