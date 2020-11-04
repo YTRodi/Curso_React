ed780f8065dfced0f29cca250dde84d5a1dad0f8
YTRodi <yagorodi@gmail.com>
2020-11-04 15:36:38 -0300

    feat: prueba async-await y Fetch

---
+++ 03-counter-app/src/tests/base/11-async-await.test.js
@@ -0,0 +1,17 @@
+import { getImagen } from '../../base/11-async-await';
+
+describe('Pruebas con async-await y Fetch', () => {
+    
+    test('Debe retornar el url de la imagen', async() => {
+        
+        const url = await getImagen();
+        // console.log( url );
+
+
+        expect( typeof url ).toBe( 'string' );
+        expect( url.includes( 'https://' ) ).toBe( true );
+        
+
+    });
+
+});
