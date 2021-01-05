3bcafc298373349f8c4357132d969d81529a3535
YTRodi <yagorodi@gmail.com>
2021-01-05 16:50:31 -0300

    implementing CRUD...

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,14 +1,15 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm';
 
 import './styles.css';
 
-const initialState = [{
-    id: new Date().getTime(),
-    description: 'Aprender React',
-    done: false
-}];
+const init = () => {
 
+    // Si hay todos me los trae, sino me trae el [] vacio.
+    return JSON.parse( localStorage.getItem( 'todos' )) || [];
+    
+}
 
 export const TodoApp = () => {
 
@@ -16,22 +17,36 @@ export const TodoApp = () => {
      * # 1 = function
      * # 2 = estado inicial
      * # 3 = function => es usado para inicializar el state en caso de que ese state haga varias acciones.
+     *                   LO QUE SEA QUE RETORNE LA FUNCION 'init' va a ser mi initialState.
      */
 
     /** ¿ QUE ES EL DISPATCH ?
      * Es una función que le mandamos una accion y ya va a saber a que reducer enviar la info.
      * Y cuando cambie el state, va a redibujar lo que cambie.
      */
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState );
+    const [ todos, dispatch ] = useReducer( todoReducer, [], init );
 
-    console.log(todos);
+    const [ { description }, handleInputChange, reset ] = useForm({
+        description: '' // Mismo name del input.
+    });
+
+    // Si los todos cambian, vuelvo a grabar en el localStorage
+    useEffect( () => {
+
+        localStorage.setItem( 'todos', JSON.stringify( todos ) );
+
+    }, [ todos ] );
 
     const handleSubmit = ( e ) => {
+
         e.preventDefault();
         
+        // Puedo retornar un componente para los administrar los errores
+        if ( description.trim().length <= 1 ) { return; }
+
         const newTodo = {
             id: new Date().getTime(),
-            description: 'Nueva tarea',
+            desc: description,
             done: false
         };
 
@@ -43,6 +58,7 @@ export const TodoApp = () => {
         // Este dispatch se puede mandar como argumento a componentes HIJOS y ya va a saber 
         // que pertenece a este reducer ( ya que son pasados por referencia. )
         dispatch( addAction );
+        reset();
     }
 
     return (
@@ -62,7 +78,7 @@ export const TodoApp = () => {
                                     className="list-group-item"
                                 >
                                     <p className="text-center">
-                                        { index + 1 } - { todo.description }
+                                        { index + 1 } - { todo.desc }
                                     </p>
 
                                     <button className="btn btn-danger">
@@ -75,7 +91,7 @@ export const TodoApp = () => {
                 </div>
 
                 <div className="col-5">
-                    <h4>Agregar TODO</h4>
+                    <h4>add TODO</h4>
                     <hr />
 
                     <form onSubmit={ handleSubmit }>
@@ -84,6 +100,9 @@ export const TodoApp = () => {
                             name="description"
                             className="form-control"
                             placeholder="Your new todo..."
+                            autoComplete="false"
+                            value={ description }
+                            onChange={ handleInputChange }
                         >
                         </input>
 
