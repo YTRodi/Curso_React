2f6a9eb7aebad5459c81a8515f08f95b52fea664
YTRodi <yagorodi@gmail.com>
2021-01-04 21:10:18 -0300

    add useMemo hook

---
+++ 05-hook-app/src/components/06-memos/MemoHook.js
@@ -0,0 +1,40 @@
+import React, { useMemo, useState } from 'react';
+import { heavyProcess } from '../../helpers/heavyProcess';
+import { useCounter } from "../../hooks/useCounter";
+
+import '../02-useEffect/effects.css';
+
+export const MemoHook = () => {
+
+    const { counter, increment } = useCounter( 1000 );
+    const [show, setShow] = useState(true);
+
+    // # 1 - callback
+    // # 2 - dependencias ( si algo cambiar, quiero memorizar el resultado. )
+    // Lógica: Si el counter cambia, necesito una nueva versión memorizada del resultado de esa función.
+    const memoProcesoPesado = useMemo( () => heavyProcess( counter ), [ counter ] );
+
+    return (
+        <div>
+            <h1>MemoHook!</h1>
+            <h3>Counter: <small>{ counter }</small></h3>
+            <hr />
+
+            <p> { memoProcesoPesado } </p>
+
+            <button
+                className="btn btn-primary"
+                onClick={ increment }
+            >
+                +1
+            </button>
+            
+            <button
+                className="btn btn-outline-primary ml-3"
+                onClick={ () => setShow( !show ) }
+            >
+                Show/Hide { JSON.stringify( show ) }
+            </button>
+        </div>
+    )
+}
