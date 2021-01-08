a4d4dfc8cde6c54e92b150e2eaf71a5fae09e7bf
YTRodi <yagorodi@gmail.com>
2021-01-08 10:55:19 -0300

    add test TodoListItem

---
+++ 05-hook-app/src/tests/components/08-useReducer/components/TodoListItem.test.js
@@ -0,0 +1,71 @@
+import { shallow } from "enzyme";
+import { TodoListItem } from "../../../../components/08-useReducer/components/TodoListItem";
+import { demoTodos } from "../../../fixtures/demoTodos";
+
+describe('Pruebas en el componente <TodoListItem />', () => {
+    
+    const handleDelete = jest.fn();
+    const handleToggle = jest.fn();
+
+    const wrapper = shallow( 
+        <TodoListItem 
+            todo={ demoTodos[0] }
+            index={ 0 }
+            handleDelete={ handleDelete }
+            handleToggle={ handleToggle }
+        /> 
+    );
+
+    test('Debe de mostrarse correctamente', () => {
+        
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('Debe de llamar a al función handleDelete', () => {
+        
+        // jest.fn()
+        // toHaveBeenCalled
+        // toHaveBeenCalledWith
+        wrapper.find( 'button' ).simulate( 'click' );
+        expect( handleDelete ).toHaveBeenCalledWith( demoTodos[0].id );
+
+    });
+
+    test('Debe de llamar a al función handleToggle', () => {
+        
+        // jest.fn()
+        wrapper.find( 'p' ).simulate( 'click' );
+        expect( handleToggle ).toHaveBeenCalledWith( demoTodos[0].id );
+
+    });
+
+    test('Debe de mostrar el texto correctamente', () => {
+        
+        // Contenido del párrafo
+        const p = wrapper.find( 'p' );
+        
+        expect( p.text().trim() ).toBe( `1 - ${ demoTodos[0].desc }` )
+
+    });
+
+    test('Debe de tener la clase complete si el TODO.done === true', () => {
+        
+        const todo = demoTodos[0];
+        todo.done = true;
+
+        const wrapper = shallow( 
+            <TodoListItem 
+                todo={ todo }
+                index={ 0 }
+                handleDelete={ handleDelete }
+                handleToggle={ handleToggle }
+            /> 
+        );
+
+        console.log(wrapper.html())
+        expect( wrapper.find( 'p' ).hasClass( 'complete' ) ).toBe( true );
+
+    });
+
+}); 
