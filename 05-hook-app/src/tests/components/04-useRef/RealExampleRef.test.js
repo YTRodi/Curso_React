8d16ec18a0172560505aaad4d1d90d438ab01e02
YTRodi <yagorodi@gmail.com>
2021-01-08 09:49:22 -0300

    add test RealExampleRef

---
+++ 05-hook-app/src/tests/components/04-useRef/RealExampleRef.test.js
@@ -0,0 +1,25 @@
+import { shallow } from "enzyme";
+import { RealExampleRef } from "../../../components/04-useRef/RealExampleRef";
+
+describe('Pruebas en el componente <RealExampleRef />', () => {
+    
+    const wrapper = shallow( <RealExampleRef /> );
+
+    test('Debe mostrarse correctamente', () => {
+        
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find( 'MultipleCustomHooks' ).exists() ).toBe( false );
+
+    });
+
+    test('Debe de mostrar el componente <MultipleCustomHooks />', () => {
+        
+        // Simulo el click
+        wrapper.find( 'button' ).simulate( 'click' );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find( 'MultipleCustomHooks' ).exists() ).toBe( true );
+
+    });
+
+});
