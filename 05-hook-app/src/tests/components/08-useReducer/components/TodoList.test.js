b4ddfbe6f9f7da7d6a09efc693459886a0d36028
YTRodi <yagorodi@gmail.com>
2021-01-08 11:06:36 -0300

    add test TodoList

---
+++ 05-hook-app/src/tests/components/08-useReducer/components/TodoList.test.js
@@ -0,0 +1,35 @@
+import { shallow } from "enzyme";
+import { TodoList } from "../../../../components/08-useReducer/components/TodoList";
+import { demoTodos } from "../../../fixtures/demoTodos";
+
+describe('Pruebas en <TodoList />', () => {
+    
+    const handleDelete = jest.fn();
+    const handleToggle = jest.fn();
+
+    const wrapper = shallow( 
+        <TodoList 
+            todos={ demoTodos }
+            handleDelete={ handleDelete }
+            handleToggle={ handleToggle }
+        /> 
+    );
+
+    test('Debe de mostrarse correctamente', () => {
+        
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('Debe de tener dos <TodoListItem />', () => {
+        
+        expect( wrapper.find( 'TodoListItem' ).length ).toBe( demoTodos.length );
+        // console.log( wrapper.find( 'TodoListItem' ).at( 0 ).props() );
+        
+        // Los componentes <TodoListItem /> que se creen deben de tener estas funciones
+        expect( wrapper.find( 'TodoListItem' ).at( 0 ).prop( 'handleToggle' )).toEqual( expect.any(Function) );
+        expect( wrapper.find( 'TodoListItem' ).at( 0 ).prop( 'handleDelete' )).toEqual( expect.any(Function) );
+
+    });
+
+});
