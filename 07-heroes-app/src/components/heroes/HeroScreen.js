74f97ef343c23f36e682e9864f09b4243302bdf5
YTRodi <yagorodi@gmail.com>
2021-01-11 22:04:35 -0300

    add animation

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -27,14 +27,15 @@ export const HeroScreen = ( { history } ) => {
     return (
         <div className="row mt-5">
             <div className="col-4">
-                <img 
+                <img
                     src={ `../assets/heroes/${ heroeId }.jpg` } 
-                    className="img-thumbnail" 
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                     alt={ alter_ego }
                 />
             </div>
 
-            <div className="col-8">
+            
+            <div className="col-8 animate__animated animate__fadeInRight">
                 <h3>{ superhero }</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item"> <b> Alter ego: </b> { alter_ego } </li>
