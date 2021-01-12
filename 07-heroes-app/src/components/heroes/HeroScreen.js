00f370faaaa561d4b166f81ad3be241fc4bc2ed1
YTRodi <yagorodi@gmail.com>
2021-01-11 21:25:02 -0300

    add HeroScreen component

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroesById } from '../../selectors/getHeroesById';
 
 export const HeroScreen = () => {
+
+    // const params = useParams();
+    const { heroeId } = useParams(); // Hook nativo de 'react-router-dom'
+
+    const hero = getHeroesById( heroeId );
+
+    if( !hero ) {
+        // Valido una ruta en caso de que ingresen cualquier cosa.
+        return <Redirect to="/" />
+    }
+
+    const { superhero, publisher, alter_ego, first_appearance, characters } = hero;
+
     return (
         <div>
             <h1>Hero Screen</h1>
