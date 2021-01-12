1e69f98f1a9d0ae902173d7a1531698bd91282c5
YTRodi <yagorodi@gmail.com>
2021-01-11 23:02:55 -0300

    app works!

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import { heroes } from '../../data/heroes'
+import React, { useMemo } from 'react';
+import queryString from 'query-string';
+
+import { useLocation } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
-export const SearchScreen = () => {
+export const SearchScreen = ( { history } ) => {
 
-    const heroesFiltered = heroes;
+    const { search } = useLocation(); // Hook nativo de 'react-router-dom'
+    const { q = '' } = queryString.parse( search );
 
-    const [ formValues, handleInputChange, reset ] = useForm( {
-        searchText: ''
+    
+    const [ formValues, handleInputChange ] = useForm( {
+        searchText: q
     });
-
+    
     const { searchText } = formValues;
+
+    // Unicamente se va a disparar esto cuando el query cambie.
+    const heroesFiltered = useMemo(() => getHeroesByName( q ), [ q ]);
 
     const handleSearch = ( e ) => {
         e.preventDefault();
 
-        console.log(searchText)
+        history.push( `?q=${ searchText }` );
     }
 
     return (
@@ -25,7 +33,7 @@ export const SearchScreen = () => {
             <hr />
 
             <div className="row">
-                <div className="col-5">
+                <div className="col-5 animate__animated animate__fadeInLeft">
                     <h4>Search Form</h4>
                     <hr />
                     <form>
@@ -49,10 +57,28 @@ export const SearchScreen = () => {
                     </form>
                 </div>
 
-                <div className="col-7">
+                <div className="col-7 animate__animated animate__fadeInRight">
 
                     <h4>Results</h4>
                     <hr />
+                    
+                    {
+                        ( q === '' )
+                        && 
+                        <div className="alert alert-info">
+                            Search a hero
+                        </div>
+                    }
+
+                    {
+                        ( q !== '' && heroesFiltered.length === 0)
+                        && 
+                        <div className="alert alert-danger">
+                            There is no a hero with '{ q }'
+                        </div>
+                    }
+                    
+                    
                     {
                         heroesFiltered.map( hero => (
                             <HeroCard 
